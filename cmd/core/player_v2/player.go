15823a03a37168fa9a468ae185d67aff49c334eb
Shreyas Kumar <shreyaskr.0914@gmail.com>
2025-07-18 23:52:34 +0530

    Final commit before refactoring

--- cmd/core/player_v2/player.go
+++ cmd/core/player_v2/player.go
@@ -1,8 +1,10 @@
-package playerv2
+package main
 
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/faiface/beep"
@@ -11,20 +13,22 @@ import (
 )
 
 func InitPlayer() {
+	l := log.New(os.Stdout, "player-v2: ", log.LstdFlags)
+	l.Println("Logging starts")
+
 	filename := "../../../samples/test_audio.mp3"
 	f, err := os.Open(filename)
 	if err != nil {
-		log.Fatal(err)
+		l.Fatal(err)
 	}
 
 	streamer, format, err := mp3.Decode(f)
 	if err != nil {
-		log.Fatal(err)
+		l.Fatal(err)
 	}
 	defer streamer.Close()
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
+	// speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
 	// NOT TO BE CALLED EVERYTIME
 	sr := format.SampleRate
@@ -38,18 +42,22 @@ func InitPlayer() {
 
 	// ctrl := &beep.Ctrl{}
 
-	done := make(chan bool)
+	done := make(chan bool, 1)
+	stop := make(chan os.Signal, 1)
 	speaker.Play(beep.Seq(resampled, beep.Callback(func() {
 		done <- true // plays the song till done is triggered
 	})))
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
 	for {
 		select {
 		case <-done:
 			return
+		case <-stop:
+			return
 		case <-time.After(time.Second):
 			speaker.Lock()
-			log.Println(format.SampleRate.D(streamer.Position()).Round(time.Second))
+			l.Println(format.SampleRate.D(streamer.Position()).Round(time.Second))
 			speaker.Unlock()
 		}
 	}
--- cmd/core/player_v2/player_test.go
+++
@@ -1,7 +0,0 @@
-package playerv2
-
-import "testing"
-
-func TestInitPlayer(t *testing.T) {
-	InitPlayer()
-}
